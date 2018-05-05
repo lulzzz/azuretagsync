d57d094608432046163f28176c37e4fa26c85985
trn@nielski.com <trn@nielski.com>
2018-05-04 22:09:59 -0500

    fix for items with null tags

--- Functions/AddTags.cs
+++ Functions/AddTags.cs
@@ -41,7 +41,7 @@ namespace AzureManagement.Function
 
             string resourceGroupName = data.SelectToken("data.context.activityLog.resourceGroupName").Value<string>();
             string resourceId = data.SelectToken("data.context.activityLog.resourceId").Value<string>();
-            string resourceType = data.SelectToken("data.context.activityLog.resourceProviderName.value").Value<string>();
+            string resourceProvider = data.SelectToken("data.context.activityLog.resourceProviderName.value").Value<string>();
             string subscriptionId = data.SelectToken("data.context.activityLog.subscriptionId").Value<string>();
  
             if (string.IsNullOrEmpty(resourceGroupName))
@@ -51,20 +51,22 @@ namespace AzureManagement.Function
             }    
 
             log.Info(message: "Resource ID is: " + resourceId);
-            log.Info(message: "Resource type is: " + resourceType);
+            log.Info(message: "Resource provider is: " + resourceProvider);
 
-            // If the resource type is listed in the invalid resources table, terminate
+            // If the resource provider is listed in the invalid resources table, terminate
+            // TODO: Consider removing this. What really matters is the resource 'type' (see later)
             var invalidTagResourcesQuery = await invalidResources.ExecuteQuerySegmentedAsync(new TableQuery<InvalidTagResource>(), null);
             InvalidTagResource matchingInvalidResource = invalidTagResourcesQuery.Results.Where(x => x.Id == resourceId).FirstOrDefault();
             if (matchingInvalidResource != null)
             {
-                log.Error("Resource is listed as invalid for tags");
+                log.Error("Resource provider is listed as invalid for tags");
                 return (ActionResult)new OkObjectResult("OK");
             }
 
-            // Get the Resource Group and its tags
             var client = new ResourceManagementClient(GetAccessToken());
             client.SubscriptionId = subscriptionId;
+
+            // Get the Resource Group and its tags
             ResourceGroup rg = client.ResourceGroups.Get(resourceGroupName);
             var rgTags = rg.Tags;
 
@@ -74,6 +76,21 @@ namespace AzureManagement.Function
                 return (ActionResult)new OkObjectResult("OK");
             }
 
+            var provider = client.Providers.Get(resourceProvider);
+            string apiVersion = provider.ResourceTypes[0].ApiVersions[0];  // Get latest API version for the resource type
+            var targetItem = client.Resources.GetById(resourceId, apiVersion);
+
+            log.Info("Item type is: " + targetItem.Type);
+
+            // See if the object type is invalid for tags. If so, terminate
+            var invalidForTagsMatch = invalidTagResourcesQuery.Results.Where(x => x.Id == targetItem.Type).FirstOrDefault();
+            if(invalidForTagsMatch != null)
+            {
+                log.Error("Resource is listed as invalid for tags");
+                return (ActionResult)new OkObjectResult("OK");
+            }
+
+            // Apply tags
             var requiredTagsQuery = await requiredTags.ExecuteQuerySegmentedAsync(new TableQuery<RequiredTag>(), null);
 
             Dictionary<string, string> chargebackTags = new Dictionary<string, string>();
@@ -85,20 +102,12 @@ namespace AzureManagement.Function
                 }
             }
 
-            var provider = client.Providers.Get(resourceType);
-            // Get latest API version for the resource type
-            string apiVersion = provider.ResourceTypes[0].ApiVersions[0];
-            var targetItem = client.Resources.GetById(resourceId, apiVersion);
-
-            // See if the object type is invalid for tags. If so, terminate
-            var invalidForTagsMatch = invalidTagResourcesQuery.Results.Where(x => x.Id == targetItem.Type);
-            if(invalidForTagsMatch != null)
+            var targetItemTags = new Dictionary<string, string>();
+            if (targetItem.Tags != null)
             {
-                log.Error("Resource is listed as invalid for tags");
-                return (ActionResult)new OkObjectResult("OK");
+                targetItemTags = (Dictionary<string, string>) targetItem.Tags;
             }
 
-            var targetItemTags = targetItem.Tags;
             foreach(var chargebackTag in chargebackTags)
             {
                 // TODO: only update with new key if it doesn't exist
